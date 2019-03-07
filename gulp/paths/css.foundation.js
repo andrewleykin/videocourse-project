3319d7d6790dc53e25e5bb8b57e7e07d0ef7e9af
Andrew Leykin <katashi1328@mail.ru>
2019-03-07 15:04:34 +0300

    fixed stylus and delete normalize

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = [
-  './node_modules/normalize.css/normalize.css',
   './node_modules/animate.css/animate.min.css',
   './node_modules/remodal/dist/remodal-default-theme.css',
   './node_modules/remodal/dist/remodal.css',
